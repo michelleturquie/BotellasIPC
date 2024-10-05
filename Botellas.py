8ec2db50e23142532de6d4fdb642e641c9da3a15
Michelle Turquie <83307186+michelleturquie@users.noreply.github.com>
2024-10-05 19:27:56 -0300

    Empecé los puntos, no sale del while cuando gana

--- Botellas.py
+++ Botellas.py
@@ -19,27 +19,37 @@ Verde = 3
 Negro = 4
 Blanco = 5
 
-
 print("Los numeros del 1 al 5 han sido ordenados aleatoriamente.")
 
 #Pide la cantidad de intentos
 print("Tenga en cuenta que el puntaje por cada botella se calcula como [10000 / intentos_totales]")
+
 intentos = input ("¿Cuantos intentos tendrá? ")
 
 #Instrucciones
-print (f"{negrita}Instrucciones:{resetear} Usted deberá adivinar el orden en " + intentos + " intentos. Por cada botella adivinado ganará 1000 puntos Por cada intento perdido perderá 100 puntos También puede pedir ayuda escribiendo help [n] donde [n] es el número que quiere revelar. Esta ayuda le revelará el n úmero a cambio de que ahora el puntaje de cada botella se divide a la mitad. Tenga en cuenta que solo puede pedir una ayuda y que también perderá un intento (-100 puntos)")
+print (f"{negrita}Instrucciones:{resetear} Usted deberá adivinar el orden en " + str(intentos) + " intentos. Por cada botella adivinado ganará 1000 puntos Por cada intento perdido perderá 100 puntos También puede pedir ayuda escribiendo help [n] donde [n] es el número que quiere revelar. Esta ayuda le revelará el n úmero a cambio de que ahora el puntaje de cada botella se divide a la mitad. Tenga en cuenta que solo puede pedir una ayuda y que también perderá un intento (-100 puntos)")
 
+# Eliminar despues, es para que chequear que funcione ok
 print(adivinanza)
 
-# Entra en el while a pedir los numeros hasta que se quede sin intentos
-
 intentos = int(intentos)
+
+#Definir puntaje base
+
+puntajebase = (10000/intentos)*5
+
+rondas = 0
+puntosfinales = 0
+
+# Entra en el while a pedir los numeros hasta que se quede sin intentos
 
 while intentos > 0:
 
     ordeningresado = input("Ingresa 5 números del 1 al 5 (sin espacios): ")
     
     intentos -= 1
+    
+    rondas += 100
     
     ordeningresadoL = [int(digito) for digito in ordeningresado]
     
@@ -53,8 +63,9 @@ while intentos > 0:
                 print("Te quedan " + str(intentos) + " intentos")
     
         if len(comparar) == 5:
+            
             print("¡Felicidades! Todos los números están en la posición correcta.")
-
+            
             break
         
         else:
