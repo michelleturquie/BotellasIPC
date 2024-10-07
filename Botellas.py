afcaa15bec64da7f3772c804e6cc3c20ec5eb7a8
Michelle Turquie <83307186+michelleturquie@users.noreply.github.com>
2024-10-07 16:26:11 -0300

    Colores

--- Botellas.py
+++ Botellas.py
@@ -5,7 +5,16 @@ Created on Sat Oct  5 17:12:25 2024
 import random
 
 negrita = '\033[1m'
+subrayado = '\033[4m'
+
+colorazul = '\033[34m'
+colorverde = '\033[32m'
+colorojo = '\033[31m'
+colorblanco = '\033[30;47m'
+colornegro = '\033[37;40m'
+
 resetear = '\033[0m'
+
 
 #Define el orden de las botellas
 
@@ -21,7 +30,7 @@ Blanco = 5
 
 #Bienvenida
 
-print(" \n¡Bienvenido al parque de diversiones!  \n\nEstara jugando a adivinar los colores con 5 botellas \nAzul = 1 Rojo = 2 Verde = 3 Negro = 4 Blanco = 5\n \nLos numeros del 1 al 5 han sido ordenados aleatoriamente.")
+print(f" \n¡Bienvenido al parque de diversiones!  \n\nEstara jugando a adivinar los colores con 5 botellas \n{colorazul} Azul = 1 {resetear} {colorojo} Rojo = 2 {resetear} {colorverde} Verde = 3 {resetear} {colornegro} Negro = 4 {colorblanco} Blanco = 5 {resetear}\n \nLos numeros del 1 al 5 han sido ordenados aleatoriamente.")
 
 #Pide la cantidad de intentos
 print("\nTenga en cuenta que el puntaje por cada botella se calcula como [10000 / intentos_totales]")
@@ -29,7 +38,7 @@ print("\nTenga en cuenta que el puntaje por cada botella se calcula como [10000 
 intentos = input (f"{negrita}¿Cuantos intentos tendrá? {resetear}")
 
 #Instrucciones
-print (f" \n \n{negrita}Instrucciones:{resetear} \nUsted deberá adivinar el orden en " + str(intentos) + " intentos. Por cada botella adivinado ganará 1000 puntos Por cada intento perdido perderá 100 puntos. \nTambién puede pedir ayuda escribiendo help [n] donde [n] es el número que quiere revelar. Esta ayuda le revelará el número a cambio de que ahora el puntaje de cada botella se divide a la mitad. Tenga en cuenta que solo puede pedir una ayuda y que también perderá un intento (-100 puntos). \n")
+print (f" \n \n{negrita}{subrayado}Instrucciones:{resetear} \nUsted deberá adivinar el orden en " + str(intentos) + " intentos. Por cada botella adivinado ganará 1000 puntos Por cada intento perdido perderá 100 puntos. \nTambién puede pedir ayuda escribiendo help [n] donde [n] es el número que quiere revelar. Esta ayuda le revelará el número a cambio de que ahora el puntaje de cada botella se divide a la mitad. Tenga en cuenta que solo puede pedir una ayuda y que también perderá un intento (-100 puntos). \n")
 
 # Eliminar despues, es para que chequear que funcione ok
 print(adivinanza)
